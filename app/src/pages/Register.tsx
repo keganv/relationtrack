d020b85191cc9a5c9504cbbcd76a7b3a832f3f02
Kegan VanSickle <keganv@keganv.com>
2024-09-30 19:15:52 -0500

    - Added first name and last name fields to Register form

--- app/src/pages/Register.tsx
+++ app/src/pages/Register.tsx
@@ -1,31 +1,39 @@
-import { FormEvent, useState } from "react";
-import { Link } from "react-router-dom";
-import Spinner from "../components/ui/Spinner";
-import useAuthContext from "../hooks/useAuthContext";
+import { FormEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../components/ui/Spinner';
+import useAuthContext from '../hooks/useAuthContext';
 
 export default function Register() {
   const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [password, setPassword] = useState('');
   const [password_confirmation, setPasswordConfirmation] = useState('');
   const [username, setUsername] = useState('');
-  const [terms, setTerms] = useState('');
+  const [terms, setTerms] = useState(false);
   const { register, errors, loading } = useAuthContext();
 
   const handleRegister = async (e: FormEvent) => {
     e.preventDefault()
-    register({ name, email, password, password_confirmation, username, terms })
+    await register({ firstName, lastName, email, password, password_confirmation, username, terms });
   }
 
   return (
-    <form className="space-y-6" method="POST" onSubmit={handleRegister}>
+    <form className="space-y-4" method="POST" onSubmit={handleRegister}>
       <div className="form-row">
-        <label htmlFor="name">Name<span className="required">*</span></label>
-        <input id="name" name="name" type="text" value={name}
-               className={`${errors.name && 'error'}`}
-               onChange={e => setName(e.target.value)}
+        <label htmlFor="firstName">First Name<span className="required">*</span></label>
+        <input id="firstName" name="firstName" type="text" value={firstName}
+               className={`${errors.firstName && 'error'}`}
+               onChange={(e) => setFirstName(e.target.value)}/>
+        {errors.firstName && <span className="error">{errors.firstName[0]}</span>}
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName">Last Name<span className="required">*</span></label>
+        <input id="lastName" name="lastName" type="text" value={lastName}
+               className={`${errors.lastName && 'error'}`}
+               onChange={e => setLastName(e.target.value)}
         />
-        {errors.name && <span className="error">{errors.name[0]}</span>}
+        {errors.lastName && <span className="error">{errors.lastName[0]}</span>}
       </div>
       <div className="form-row">
         <label htmlFor="username">Username<span className="required">*</span></label>
@@ -59,22 +67,20 @@ export default function Register() {
         />
       </div>
       <div className="form-row">
-        <div className="flex">
-          <input id="agree_confirm" type="checkbox" name="terms" className="w-auto mr-sm"
-                 onChange={e => setTerms(e.target.value)} />
-          <label htmlFor="agree_confirm" className="pointer">
-            I agree with <Link to={'/terms'}>the terms</Link>
-          </label>
-        </div>
-        {errors.terms && <span className="error">{errors.terms[0]}</span>}
+        <label htmlFor="agree_confirm" className="inline">
+          <input id="agree_confirm" type="checkbox" name="terms" className="mr-1"
+                 onChange={e => setTerms(e.target.checked)}/>
+          I agree with <Link to={'/terms'}>the terms</Link>
+        </label>
+        {errors.terms && <div className="flex-1 w-full error">{errors.terms[0]}</div>}
       </div>
-      <div className="flex wrap align-middle justify-between margin-top">
-        <button type="submit" className="button primary col-12-sm" disabled={loading}>
-          <span className={`${loading && 'mr-sm'}`}>Register</span>
-          <Spinner loading={loading} />
+      <div className="flex flex-wrap justify-between items-center">
+        <button type="submit" className="button primary angle-right" disabled={loading}>
+          <span className={`${loading && 'mr-2'}`}>Register</span>
+          <Spinner loading={loading}/>
         </button>
-        <div className="col-12-sm font-sm text-right text-center-sm mt-4-sm">
-          Already have an account? <Link to={'/login'}>Sign in.</Link>
+        <div className="mt-6 sm:mt-0 text-xs">
+          Already have an account? <Link to={'/login'} className="link">Sign in.</Link>
         </div>
       </div>
     </form>
