6ca0129d4752b1ac2a0dffe79caf97b5c47279b6
Kegan VanSickle <keganv@keganv.com>
2024-09-30 19:06:41 -0500

    - Package updates - Unit tests for Register form

--- app/src/__tests__/App.test.tsx
+++ app/src/__tests__/App.test.tsx
@@ -5,12 +5,12 @@ import App from '../App';
 
 describe('App', () => {
     beforeEach(() => {
-        render(
-          <Router>
-            <App />
-          </Router>
-        );
-      });
+      render(
+        <Router>
+          <App />
+        </Router>
+      );
+    });
 
     it('should show the welcome screen', async () => {
         // You can check for specific text, elements, or other content in your App component
@@ -21,10 +21,10 @@ describe('App', () => {
     it('should navigate to the login page when the login button is clicked', () => {
         // Get the login button element
         const loginButton = screen.getAllByRole('link', { name: /log in/i })[0];
-    
+
         // Simulate a click on the login button
         fireEvent.click(loginButton);
-    
+
         // Check that the URL has changed to the login page
         expect(window.location.pathname).toBe('/login');
       });
---
+++ app/src/__tests__/Register.test.tsx
@@ -0,0 +1,56 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Register from '../pages/Register';
+import useAuthContext from '../hooks/useAuthContext';
+import AuthProvider from '../providers/AuthProvider';
+
+// Mock the Spinner component and useAuthContext hook
+jest.mock('../hooks/useAuthContext');
+
+describe('Register Component', () => {
+  const mockRegister = jest.fn();
+
+  beforeEach(() => {
+    (useAuthContext as jest.Mock).mockReturnValue({
+      register: mockRegister,
+      errors: {},
+      loading: false,
+    });
+    render(
+      <MemoryRouter>
+        <AuthProvider>
+          <Register />
+        </AuthProvider>
+      </MemoryRouter>
+    );
+  });
+
+  it('renders the form correctly', () => {
+    expect(screen.getByLabelText(/First Name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Last Name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Username/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText('Password*', { exact: true })).toBeInTheDocument();
+    expect(screen.getByLabelText(/Confirm Password/i, { exact: true })).toBeInTheDocument();
+  });
+
+  it('submits the form data', () => {
+    fireEvent.change(screen.getByLabelText(/First Name/i), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText(/Last Name/i), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText(/Username/i), { target: { value: 'johndoe' } });
+    fireEvent.change(screen.getByLabelText(/Email/i), { target: { value: 'john@example.com' } });
+    fireEvent.change(screen.getByLabelText('Password*', { exact: true }), { target: { value: 'password123' } });
+    fireEvent.change(screen.getByLabelText(/Confirm Password/i), { target: { value: 'password123' } });
+    fireEvent.click(screen.getByRole('button', { name: /Register/i }));
+
+    expect(mockRegister).toHaveBeenCalledWith({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      username: 'johndoe',
+      terms: true,
+    });
+  });
+});
