2fcd8a08a339dc22f3b0b1645337649dc50411b7
Kegan VanSickle <keganv@keganv.com>
2024-09-30 19:16:45 -0500

    - Throw an error if a component tries to use the useAuthContext hook outside of the AuthProvider

--- app/src/hooks/useAuthContext.ts
+++ app/src/hooks/useAuthContext.ts
@@ -2,5 +2,11 @@ import { useContext } from "react";
 import AuthContext, { AuthContextValues } from "../contexts/AuthContext";
 
 export default function useAuthContext(): AuthContextValues {
-  return useContext(AuthContext)
+  const context = useContext(AuthContext);
+
+  if (context === undefined) {
+    throw new Error("useAuthContext must be used within a AuthProvider");
+  }
+
+  return context;
 }
