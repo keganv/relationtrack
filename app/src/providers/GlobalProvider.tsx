267d6a032fafc75cb8690da2cf102581572000ec
Kegan VanSickle <keganv@keganv.com>
2024-11-04 20:54:47 -0600

    - GlobalContextProvider revisions

--- app/src/providers/GlobalProvider.tsx
+++ app/src/providers/GlobalProvider.tsx
@@ -1,25 +1,35 @@
 import { ReactNode, useCallback, useState, } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 import { Status } from "../types/Status";
+import { AxiosError } from "axios";
 
 const DISALLOWED_STATUS_MESSAGES = ['sql', 'connection', 'select', 'from', 'where'];
+const UNAUTHORIZED_STRINGS = ['unauthenticated', 'unauthorized', 'csrf'];
 
 type GlobalProviderProps = { children: ReactNode; }
 
 const GlobalProvider = ({ children }: GlobalProviderProps) => {
+  const [doLogout, setDoLogout] = useState(false);
   const [status, setStatus] = useState<Status | null>(null);
 
-  const handleError = useCallback((e: AxiosError | unknown) => {
+  const handleError = useCallback((e: AxiosError | unknown, setErrorsFn?: (arg: object[]) => void) => {
     if (e instanceof AxiosError) {
       const message = e.response?.data.message || e.message;
-      const allowMessage = !DISALLOWED_STATUS_MESSAGES.find(status => message.includes(status));
-      setErrors(e.response?.data.errors || { error: [e.message] });
+      const allowMessage = !DISALLOWED_STATUS_MESSAGES.some(status => message.toLowerCase().includes(status));
+      const unauthorized = UNAUTHORIZED_STRINGS.some(str => message.toLowerCase().includes(str));
+      if (unauthorized && (e.status === 401)) {
+        setDoLogout(true);
+        return setStatus({ type: 'error', message: 'Your session has ended. Please log back in.'});
+      }
+      if (setErrorsFn) {
+        setErrorsFn(e.response?.data.errors || {error: [e.message]});
+      }
       setStatus({ type: 'error', message: allowMessage ? message : 'Uh oh! Something went wrong.' });
     }
-  }, [setErrors, setStatus]);
+  }, [setStatus]);
 
   return (
-    <GlobalContext.Provider value={{ handleError, setStatus }}>
+    <GlobalContext.Provider value={{ doLogout, handleError, status, setStatus }}>
       {children}
     </GlobalContext.Provider>
   );
