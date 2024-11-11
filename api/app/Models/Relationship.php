fe29ebd3437b38a6e5b3ba2c87edebd2bea9d0a3
Kegan VanSickle <keganv@keganv.com>
2024-11-10 20:49:20 -0600

    - Added a type mutator and accessor to append the attribute to the Relationship model

--- api/app/Models/Relationship.php
+++ api/app/Models/Relationship.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Relationship extends Model
@@ -14,36 +17,46 @@ class Relationship extends Model
     public $incrementing = false;
 
     protected $fillable = [
-        'type_id',
+        'type',
         'name',
         'title',
         'birthday',
         'description',
         'health',
-        'primary_image'
+        'primary_image_id'
     ];
 
-    public function user()
+    protected $appends = ['type'];
+
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function type()
+    public function relationshipType(): BelongsTo
     {
         return $this->belongsTo(RelationshipType::class, 'type_id');
     }
 
-    public function primaryImage()
+    protected function type(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $this->relationshipType()->getResults(),
+            set: fn ($value) => ['type_id' => $value],
+        );
+    }
+
+    public function primaryImage(): BelongsTo
     {
         return $this->belongsTo(File::class, 'primary_image_id');
     }
 
-    public function files()
+    public function files(): HasMany
     {
         return $this->hasMany(File::class, 'relationship_id');
     }
 
-    public function actionItems()
+    public function actionItems(): HasMany
     {
         return $this->hasMany(ActionItem::class, 'relationship_id');
     }
