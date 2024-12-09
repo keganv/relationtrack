015c2924c0af796f0f27cc5fc222e7f00495cf11
Kegan VanSickle <keganv@keganv.com>
2024-12-09 17:43:54 -0600

    Make migrations to update the sessions and action_items table for user_id - Changes the user_id columns to uuid

---
+++ api/database/migrations/2024_12_09_005149_modify_sessions_table_user_to_uuid.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->dropIndex('sessions_user_id_index');
+            $table->uuid('user_id')->nullable()->change();
+            $table->index('user_id');
+        });
+    }
+};
