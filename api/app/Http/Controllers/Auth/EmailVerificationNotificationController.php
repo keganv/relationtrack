5907110785e4e1b8f45a4a03bab72e3498d4f6d5
Kegan VanSickle <keganv@keganv.com>
2024-12-09 18:20:37 -0600

    Remove unused use statements

--- api/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ api/app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class EmailVerificationNotificationController extends Controller
 {
