05a29b877abf757fd76ec5512bd0bf8352d030b4
Kegan VanSickle <keganv@keganv.com>
2025-02-24 20:03:04 -0600

    Migrate the notes table to cascade delete from action_items relations and add the reminders relation

---
+++ api/database/migrations/2025_02_25_013210_update_notes_table_relations.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->foreignId('action_item_id')->change()->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('reminder_id')->after('action_item_id')->nullable()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+        });
+    }
+};
