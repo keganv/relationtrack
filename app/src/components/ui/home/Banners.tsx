13b55d812f0b946885c90c77773b7e014e1f5b4d
Kegan VanSickle <keganv@keganv.com>
2025-03-26 18:06:20 -0500

    Used preferred if else instead of && to avoid potential issues

--- app/src/components/ui/home/Banners.tsx
+++ app/src/components/ui/home/Banners.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 import useAuthContext from "../../../hooks/useAuthContext.ts";
 
 export default function Banners() {
-  const { logout, user } = useAuthContext();
+  const { logout, authenticated } = useAuthContext();
   const handleLogout = async (e: FormEvent) => {
     e.preventDefault();
     await logout();
@@ -18,16 +18,18 @@ export default function Banners() {
             <div className="banner-text">Keep track of what really matters.</div>
             <div className="banner-text mt-2"><em>Your Relationships!</em></div>
             <nav className="actions">
-              {user && <>
-                <Link to="/dashboard" className="button red transparent">Dashboard</Link>
-                <button type="submit" className="button white transparent" onClick={handleLogout}>
-                  <i className="fa-solid fa-right-from-bracket"></i> Log Out
-                </button>
-              </>}
-              {!user && <>
-                <Link to="/register" className="button red transparent">Register</Link>
-                <Link to="/login" className="button white transparent">Log in</Link>
-              </>}
+              {authenticated ?
+                <>
+                  <Link to="/dashboard" className="button red transparent">Dashboard</Link>
+                  <button type="submit" className="button white transparent" onClick={handleLogout}>
+                    <i className="fa-solid fa-right-from-bracket"></i> Log Out
+                  </button>
+                </> :
+                <>
+                  <Link to="/register" className="button red transparent">Register</Link>
+                  <Link to="/login" className="button white transparent">Log in</Link>
+                </>
+              }
             </nav>
           </div>
         </div>
