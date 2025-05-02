64ed24c479892c97a302e61a25d2c12b63e928e5
Kegan VanSickle <keganv@keganv.com>
2025-05-01 20:00:01 -0500

    Use the EmailFrequency type in the database migration

--- api/database/migrations/2025_04_29_231808_create_user_settings_table.php
+++ api/database/migrations/2025_04_29_231808_create_user_settings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Types\EmailFrequency;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
             $table->boolean('notifications')->default(false)->nullable(false);
-            $table->string('frequency')->default('n');
+            $table->string('email_frequency')->default(EmailFrequency::Never);
             $table->timestamps();
         });
     }
