bd5f02c259f1274e5b35c6d9e409f8aaf129cff9
Kegan VanSickle <keganv@keganv.com>
2025-05-01 19:25:09 -0500

    Remove the unneeded functionality from the AuthServiceProvider since Policies are autoloading since 11

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -8,22 +8,10 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        'App\Models\User' => 'App\Policies\UserPolicy',
-        'App\Models\Relationship' => 'App\Policies\RelationshipPolicy',
-    ];
-
-    /**
      * Register any authentication / authorization services.
      */
     public function boot(): void
     {
-        $this->registerPolicies();
-
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
