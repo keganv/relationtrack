bbd0f41f8ea6f0f7fece53fb10b7dd695f00494f
Kegan VanSickle <keganv@keganv.com>
2025-05-16 09:09:37 -0500

    Create utility function for custom iteration keys

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function createUniqueKey(val: string, length: number) {
+  return `${btoa(val).substring(0, length)}`;
 }
