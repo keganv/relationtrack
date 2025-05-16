8261cfeb9c44ed26d46b127935130fc7acc42b7a
Kegan VanSickle <keganv@keganv.com>
2025-05-16 09:09:22 -0500

    Update User types, create UserFormData

--- app/src/types/User.ts
+++ app/src/types/User.ts
@@ -1,5 +1,8 @@
+import type { NonUndefined } from 'react-hook-form';
+
 import type { ApiFile } from './ApiFile';
 import type { Relationship } from './Relationship';
+import { type UserSettings } from './UserSettings';
 
 export type User = {
   first_name?: string;
@@ -13,6 +16,9 @@ export type User = {
   email_verified_at?: string;
   profile_image?: ApiFile|File; // ApiFile when fetched from the API, File when uploaded
   relationships?: Relationship[];
+  settings: UserSettings;
 }
 
-export type UserFormData = Partial<User> | Partial<User> & File;
+// Partial<NonUndefined<User>>: a property can be omitted, but if present, it cannot be undefined
+type UserFormBase = Omit<NonUndefined<User>, 'settings' | 'relationships'>;
+export type UserFormData = Partial<UserFormBase> & UserSettings;
